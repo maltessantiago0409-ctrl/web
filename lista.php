465cda20834abadf224f671a359d612749819942
maltessantiago0409-ctrl <maltessantiago0409@gmail.com>
2026-09-13 17:51:30 -0500

    cambio finla

--- lista.php
+++ lista.php
@@ -53,6 +53,100 @@ $sql = "SELECT documento, nombre, telefono, correo, fecha FROM usuarios";
     </table>
     <br><br>
     <a style="color: white;" href="usuarios.html">Volver al formulario</a>
+
+<style>
+
+body {
+    font-family: Arial, sans-serif;
+    background-color: black;
+    color: white;
+    margin: 0;
+    padding: 40px 20px;
+    min-height: 100vh;
+}
+
+/* Titulo */
+
+h1 {
+    text-align: center;
+    color: white;
+    font-size: 40px;
+    margin-bottom: 40px;
+}
+
+/* Tabla */
+
+table {
+    width: 90%;
+    max-width: 1000px;
+    margin: 0 auto;
+    border-collapse: collapse;
+    background-color: #111;
+    border: 1px solid white;
+}
+
+/* Encabezados */
+
+th {
+    background-color: #333;
+    color: white;
+    padding: 15px;
+    text-align: center;
+    font-size: 17px;
+    border: 1px solid white;
+}
+
+/* Datos */
+
+td {
+    padding: 15px;
+    text-align: center;
+    color: white;
+    border: 1px solid #555;
+    font-size: 16px;
+}
+
+/* Efecto al pasar el mouse */
+
+tr:hover {
+    background-color: #222;
+}
+
+/* Mensaje cuando no hay clientes */
+
+td[colspan] {
+    padding: 20px;
+    text-align: center;
+    color: white;
+    font-size: 17px;
+}
+
+/* Boton volver */
+
+a {
+    display: block;
+    width: 200px;
+    margin: 35px auto;
+    padding: 12px;
+    text-align: center;
+    background-color: black;
+    color: black;
+    text-decoration: none;
+    border-radius: 8px;
+    font-size: 16px;
+    border: 2px solid white;
+}
+
+/* Efecto del boton */
+
+a:hover {
+    background-color: #cccccc;
+}
+
+</style>
+
+   
+    
     </center>
 
 
